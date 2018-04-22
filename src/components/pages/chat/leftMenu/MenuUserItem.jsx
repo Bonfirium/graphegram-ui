488ddafe57e0cc28228c194d31d07cc5b08b5e7f
Nick Borsuk <gor_nick987@Mail.ru>
2018-04-22 10:54:46 +0300

    multidialogs chats window

--- src/components/pages/chat/leftMenu/MenuUserItem.jsx
+++ src/components/pages/chat/leftMenu/MenuUserItem.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default class MenuUserItem extends React.Component {
 	render() {
+		const { name } = this.props.info;
 		return (
 			<li>
 				<div className="info">
-					<div className="user">{this.props.info.name}</div>
+					<div className="user"onClick={this.props.selectDialog} id={name}>{ name }</div>
 				</div>
 			</li>
 		);
